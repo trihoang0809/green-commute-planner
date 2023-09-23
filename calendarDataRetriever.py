2605a3d29ebcc42b1a437af16991ba1dd65af4ea
GiraffeBryce <bsl5@rice.edu>
2023-09-23 05:58:49 -0500

    Separate code into packages

---
+++ calendarDataRetriever.py
@@ -0,0 +1,31 @@
+import datetime
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+def getWeekLocations(creds):
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        later = (datetime.datetime.utcnow() + datetime.timedelta(days=7)).isoformat() + 'Z'
+        print('Getting all events from now to 7 days later...')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                              timeMax=later, singleEvents=True,
+                                              orderBy='startTime').execute()
+        events = events_result.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+            return
+
+        # Prints the locations of the events for the week
+        locations = []
+        for event in events:
+            # start = event['start'].get('dateTime', event['start'].get('date'))
+            # print(start, event['location'])
+            locations.append(event['location'])
+        return locations
+        
+    except HttpError as error:
+        print('An error occurred: %s' % error)
