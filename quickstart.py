b8533ea0db7ff911b750724dd52ad84801407d24
4Gleneyre <33353768+4Gleneyre@users.noreply.github.com>
2023-09-23 03:27:50 -0500

    make calendar API return all events in next 7 days with location

--- quickstart.py
+++ quickstart.py
@@ -12,18 +12,10 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-
 def main():
-    """Shows basic usage of the Google Calendar API.
-    Prints the start and name of the next 10 events on the user's calendar.
-    """
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -31,32 +23,45 @@ def main():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
     try:
         service = build('calendar', 'v3', credentials=creds)
 
-        # Call the Calendar API
-        now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-        print('Getting the upcoming 10 events')
-        events_result = service.events().list(calendarId='primary', timeMin=now,
-                                              maxResults=10, singleEvents=True,
-                                              orderBy='startTime').execute()
+        # Get events for the next 7 days
+        now = datetime.datetime.utcnow()
+        end = now + datetime.timedelta(days=7)
+        now_str = now.isoformat() + 'Z'
+        end_str = end.isoformat() + 'Z'
+
+        print('Getting events for the next 7 days')
+        events_result = service.events().list(
+            calendarId='primary',
+            timeMin=now_str,
+            timeMax=end_str,
+            singleEvents=True,
+            orderBy='startTime'
+        ).execute()
+
         events = events_result.get('items', [])
 
         if not events:
             print('No upcoming events found.')
             return
 
-        # Prints the start and name of the next 10 events
         for event in events:
             start = event['start'].get('dateTime', event['start'].get('date'))
-            print(start, event['summary'])
+            event_location = event.get('location', 'Location not specified')
+            print(f"{start}, {event['summary']}, Location: {event_location}")
 
     except HttpError as error:
-        print('An error occurred: %s' % error)
+        print(f'An error occurred: {error}')
+
+
+if __name__ == '__main__':
+    main()
+
 
 
 if __name__ == '__main__':
