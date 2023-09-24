d2e8a8e5f7d22ec837cda864b320ef74549b1aad
4Gleneyre <33353768+4Gleneyre@users.noreply.github.com>
2023-09-24 05:20:19 -0500

    delete unncessary files

--- test.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-# Create a select box with some options
-selected_option = st.selectbox("Select an option", ["Option 1", "Option 2", "Option 3"])
-
-# Display the selected option
-st.write("You selected:", selected_option)
