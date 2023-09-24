ee674608d2eaecbdc9208f0590473b43e65d9d09
t0dd26 <145819025+t0dd26@users.noreply.github.com>
2023-09-23 19:21:07 -0500

    Update planner.py

--- planner.py
+++ planner.py
@@ -5,7 +5,6 @@ import os.path
 import calendarDataRetriever
 import auth
 import streamlit as st
-import maps
 from streamlit.logger import get_logger
 
 from google.auth.transport.requests import Request
@@ -17,59 +16,71 @@ from googleapiclient.errors import HttpError
 
 LOGGER = get_logger(__name__)
 
-def run():    
+    
+def main():
+    # Set up config for website? (Authenticate the user in order to pull their data for Google Calendar API.)
     st.set_page_config(
         page_title="Green Commute",
         page_icon="🛴",
     )
 
     st.write("# Green Commute Planner")
-    
-    print("HELLO")
+
+    creds = None
 
     if st.button("Log in", type="primary"):
         creds = auth.userAuthorization()
-        print("HELLO")
-        return creds
+        print("creds initialized")
+    # elif not creds:
+    #    st.write("Please log in")
+
+    # weekLocations = None
     
-def main():
-    # Set up config for website? (Authenticate the user in order to pull their data for Google Calendar API.)
-    creds = run()
+    # if weekLocations:
+    #     display_events(weekLocations)
+
+    # Button to refresh the calendar
+    if st.button("Refresh Calendar", type="primary"):
+        creds = auth.userAuthorization()
+        weekLocations = calendarDataRetriever.getWeekLocations(creds)
+        print("calendar will be refreshed...")
+
+        # Display locations on website.
+        if not creds:
+            display_events(weekLocations)
 
     # Get all locations in Google Calendar for until the next week from now.
-    weekLocations = calendarDataRetriever.getWeekLocations(creds)
-    print(weekLocations)
-    
-    # Get all paths for the week and calculate the path distance.
-    pathDistances = []
-    for i in range(len(weekLocations)-1):
-        fromLoc, toLoc = weekLocations[i], weekLocations[i+1]
-        dist = maps.getPathDistance(fromLoc, toLoc)
-        pathDistances.append((toLoc[1], fromLoc[0], toLoc[0], dist)) # (toStartTime, fromEventName, toEventName, dist)
+    if creds:
+        print("getting locations...")
+        weekLocations = calendarDataRetriever.getWeekLocations(creds)
+        print("locations gotten: ", weekLocations)
         
-    # Display path distances on site.
-    display_events(pathDistances)
+        # Display locations on website.
+        display_events(weekLocations)
 
     transportation = st.selectbox(
     'Which method of transportation do you log?',
     ('Walk', 'Bike', 'Bus/Train', 'Car', 'Plane'), placeholder="Choose an Option")
+    if transportation!=None:
+        creds = auth.userAuthorization()
+        weekLocations = calendarDataRetriever.getWeekLocations(creds)
+        display_events(weekLocations)
+
     if st.button("Log", type="primary"):
         # Do something else
+        print("transportation has been chosen...")
         st.write(transportation)
 
-def display_events(pathDistances):  # [(toStartTime, fromEventName, toEventName, dist)]
-    if not pathDistances:
+def display_events(weekLocations):
+    if not weekLocations:
         st.write("No upcoming events found.")
         return
     # Loop through events and display them
-    for path in pathDistances: # loc is of form (eventName, UTCTime, location)
-        st.write(f"**Time When User Finishes Path:** {path[0]}")
-        st.write(f"**Starting Location:** {path[1]}")
-        st.write(f"**Ending Location:** {path[2]}")
-        st.write(f"**Path Distance:** {path[3]}")
+    for loc in weekLocations: # loc is of form (eventName, UTCTime, location)
+        st.write(f"**Event:** {loc[0]}")
+        st.write(f"**Start Time:** {loc[1]}")
+        st.write(f"**Location:** {loc[2]}")
         st.write("---")
 
 if __name__ == '__main__':
-    # Check if 'started' key exists in session_state, if not, run main.
-    if not st.session_state.get('started'):
-        main()
+    main()
